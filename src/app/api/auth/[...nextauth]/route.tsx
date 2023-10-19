609c5f44f6abb67bf036d0190528e1f522277e72
KevKiragu <kevkiragu96@gmail.com>
2023-10-19 10:02:31 +0300

    Errors Resolved and Comments update

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -1,7 +1,8 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import axios from "axios"
+import axios from "axios";
 
+// For definition of User interface with the expected user properties
 interface User {
   id: number;
   username: string;
@@ -13,16 +14,18 @@ interface User {
   exp: number;
   jti: string;
 }
-const https=require('https')
 
+const https = require('https');
 
+// Configuration of the NextAuth handler
 const handler = NextAuth({
   session: {
     strategy: "jwt",
 
+    // Configuration of the user session expiration as well as update
     // Seconds - How long until an idle session expires and is no longer valid.
     maxAge: 30 * 24 * 60 * 60, // 30 days
-  
+
     // Seconds - Throttle how frequently to write to database to extend a session.
     // Use it to limit write operations. Set to 0 to always update the database.
     // Note: This option is ignored if using JSON Web Tokens
@@ -31,26 +34,34 @@ const handler = NextAuth({
   providers: [
     CredentialsProvider({
       name: "Credentials",
+
+      // Definition of credentials required for purposes of authentication
       credentials: {
-        username: { label: "username", type: "text", name:"userName" },
+        username: { label: "username", type: "text", name: "userName" },
         password: { label: "password", type: "password" },
       },
       async authorize(credentials) {
+        if (!credentials) {
+          return null; // Case handling when credentials are undefined
+        }
         const { username, password } = credentials as {
           username: string;
           password: string;
         };
-        const res =await axios.post("https://localhost:7279/api/v1/users/login", {
-            userName:credentials.username,
-            password:credentials.password
+
+        // Send a POST request to authentication endpoint
+        const res = await axios.post("https://localhost:7279/api/v1/users/login", {
+          userName: credentials.username,
+          password: credentials.password,
         }, {
-            httpsAgent: new https.Agent({ rejectUnauthorized: false }),
-        })  
-        // tipar peticion de user
+          httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+        });
+
+        // Extraction of user data from the response
         const user = res.data;
-        console.log(res.status)
+        console.log(res.status);
         if (user) {
-          // mostrar el encabezado de autorización en la consola
+          // Return the user if authentication is successful
           return user;
         } else {
           return null;
@@ -62,15 +73,15 @@ const handler = NextAuth({
     jwt: async ({ token, user }) => {
       if (user) token = user as unknown as { [key: string]: any };
       console.log(token);
-      
+
       return token;
     },
     session: async ({ session, token }) => {
-      session.user = { ...token }
+      session.user = { ...token };
       return session;
     },
   },
-  secret: "supersecret",
+  secret: "supersecret", //secret key for NextAuth
   pages: {
     signIn: "/login",
     error: '/login',
