dc37499fbafea982dad8414a921e687146c568bc
KevKiragu <kevkiragu96@gmail.com>
2023-10-19 10:35:07 +0300

    Error Fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,3 @@
-// "use client"
-// import React, { useEffect, useState } from 'react';
-
-// import { useParams } from 'next/navigation';
-// import Link from 'next/link';
-
-// import Breadcrumbs from '@mui/material/Breadcrumbs';
-// import InfoRoundedIcon from '@mui/icons-material/InfoRounded';
-// import Dashboard from '@/components/dashboards';
-
-// const page = () => {
-//   const { slug } = useParams();
-
-//   useEffect(() => {
-//     // Use slug here or access specific properties if 'slug' is an object
-//     console.log(slug);
-//   }, [slug]); // Re-run the effect whenever 'slug' changes
-
-//   return (
-//     <div style={{ height: "auto" }}>
-//       <main className='min-h-screen bg-[#ededed] lg:mx-20 sm:mx-0'>
-
-//         {/* our content  */}
-//         {slug === undefined ? <Dashboard/>: ""}
-
-//       </main>
-//     </div>
-//   );
-// };
-
-// export default page
 "use client"
 import withAuth from "@/utils/PrivateRouting";
 import dashboard from "./dashboard/dashboard";
