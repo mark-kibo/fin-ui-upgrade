5c229f80fb4cf507cc04d4749aa1be7d1a0365a2
KevKiragu <kevkiragu96@gmail.com>
2023-10-23 17:51:06 +0300

    Validation for User Inputs

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,6 +1,64 @@
-import React from 'react'
+"use client"
+import React,{ useState, useEffect } from 'react'; 
 import Link from 'next/link'
-function signup() {
+const signup = () => { 
+	const [firstName, setFirstName] = useState(''); 
+	const [lastName, setLastName] = useState(''); 
+	const [email, setEmail] = useState(''); 
+	const [phoneNumber, setPhoneNumber] = useState('');
+	const [requestNote, setRequestNote] = useState('');
+	const [errors, setErrors] = useState({}); 
+	const [isFormValid, setIsFormValid] = useState(false); 
+	
+
+	useEffect(() => { 
+		validateForm(); 
+	}, [firstName,lastName, email, phoneNumber,requestNote]); 
+	// Validate form 
+	const validateForm = () => { 
+		let errors = {}; 
+
+		if (!firstName) {
+			errors.firstName = '';  // Leave it empty
+		} else if (!/^[A-Za-z\s]+$/.test(firstName)) {
+			errors.firstName =  'First name is invalid !';
+		}
+		
+
+		if (!lastName) {
+			errors.lastName = '';  // Leave it empty
+		} else if (!/^[A-Za-z\s]+$/.test(lastName)) {
+			errors.lastName = 'Last name is invalid';
+		}
+
+		if (!email) {
+			errors.email = '';  // Leave it empty
+		} else if (!/\S+@\S+\.\S+/.test(email)) {
+			errors.email = 'This is not a valid email address';
+		}
+		if (!phoneNumber) {
+    errors.phoneNumber = '';  // Leave it empty
+} else if (!/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/.test(phoneNumber)) {
+    errors.phoneNumber = 'Phone number is not in the correct format (e.g., 123-456-7890)';
+}
+
+		
+		setErrors(errors); 
+		setIsFormValid(Object.keys(errors).length === 0); 
+	}; 
+	// Handle input with validation
+  
+  
+	// Submit 
+	const handleSubmit = () => { 
+		if (isFormValid) { 
+			console.log('Form submitted successfully!'); 
+		} else { 
+			console.log('Sign up Form has errors. Please correct them.'); 
+		} 
+	}; 
+
+
     return (
         <main className="flex min-h-screen flex-col sm:w-2/2  justify-center items-center h-screen bg-white">
             <div className='w-1/3  text-[#1F5780] shadow-2xl p-5 rounded-2xl '>
@@ -10,21 +68,67 @@ function signup() {
                 <p className='font-bold mb-5 text-[#1F5780]'>Sign Up</p>
                 <form>
                     <div className="grid gap-4 mb-2 sm:grid-cols-2 ">
-                        <div>
-                            <label htmlFor="name" className="block mb-2 text-[#1F5780] text-sm font-medium dark:text-white">First Name</label>
-                            <input type="text" name="name" id="name" required className="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="John" />
-                        </div>
+					<div>
+  <label htmlFor="name" className="block mb-2 text-[#1F5780] text-sm font-medium dark:text-white">
+    First Name
+  </label>
+  <input
+    onChange={(e) => setFirstName(e.target.value)}
+    type="text"
+    name="name"
+    id="name"
+    required
+    className={`bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 ${
+      errors.firstName ? 'border-red-500 text-red-500 text-sm' : ''
+    }`}
+    placeholder="John"
+  />
+  {errors.firstName && (
+    <p className="text-red-500 text-sm">{errors.firstName}</p>
+  )}
+</div>
+
+
                         <div>
                             <label htmlFor="name" className="block mb-2 text-sm text-[#1F5780] font-medium dark:text-white">Last Name</label>
-                            <input type="text" name="name" id="name" required className="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Doe" />
+                           
+							<input
+    onChange={(e) => setLastName(e.target.value)}
+    type="text"
+    name="name"
+    id="name"
+    required
+    className={`bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 ${
+      errors.lastName ? 'border-red-500 text-red-500 text-sm' : ''
+    }`}
+    placeholder="Doe"
+  />
+  {errors.lastName && (
+    <p className="text-red-500 text-sm">{errors.lastName}</p>
+  )}
                         </div>
                         <div className='mb-1'>
                             <label htmlFor="name" className="block mb-2 text-[#1F5780] text-sm font-medium dark:text-white">Email</label>
-                            <input type="email" name="email" id="email" required className="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="johndoe@gmail.com" />
+							<input
+    onChange={(e) => setEmail(e.target.value)}
+    type="text"
+    name="name"
+    id="name"
+    required
+    className={`bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 ${
+      errors.email ? 'border-red-500 text-red-500 text-sm' : ''
+    }`}
+    placeholder="johndoe@gmail.com"
+  />
+  {errors.email && (
+    <p className="text-red-500 text-sm">{errors.email}</p>
+  )}
+
                         </div>
                         <div className='mb-1'>
                             <label htmlFor="phone" className="block mb-2 text-sm  text-[#1F5780] font-medium dark:text-white">Phone</label>
-                            <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required placeholder='070000000' className="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" />
+                            <input onChange={(e) => setPhoneNumber(e.target.value)} type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required placeholder='070000000' className="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" />
+							{errors.phoneNumber  && <p className='text-red'>{errors.phoneNumber }</p>}
                         </div>
                         <div className='mb-1'>
                             <label htmlFor="organization" className="block mb-2 text-sm text-[#1F5780]  font-medium dark:text-white">Institution Name</label>
@@ -63,9 +167,9 @@ function signup() {
                     </div>
                     <div>
                         <label htmlFor="location" className="block mb-2 text-sm text-[#1F5780] font-medium  dark:text-white">Request Note</label>
-                        <textarea name="request" id="" className="bg-gray-50 border mb-4 border-gray-300  text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"></textarea>
+                        <textarea name="request" id="" className="bg-gray-50 border mb-4 border-gray-300  text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 " ></textarea>
                     </div>
-                    <Link href='/login'><button className='bg-[#1F5780] rounded-md p-3 sm:col-span-2 w-full text-white' type="submit">Sign Up</button></Link>
+                    <Link href='/login'><button className='bg-[#1F5780] rounded-md p-3 sm:col-span-2 w-full text-white' type="submit" >Sign Up</button></Link>
                 </form>
                 <p className='flex items-center: md:flex-row justify-center items-center cursor-pointer inline-block align-bottom mt-5'>
                 <a>Already have an account? </a><Link href='/login'><span className='font-bold'>Login</span></Link>
